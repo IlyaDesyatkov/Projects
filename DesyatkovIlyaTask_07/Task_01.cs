506b6bb4da9353a212830b7422f42c2059ccb827
IlyaDesyatkov <ilyades95@gmail.com>
2016-02-11 12:59:18 +0400

    11

---
+++ DesyatkovIlyaTask_07/Task_01.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesyatkovIlyaTask_07
+{
+    class Task_01
+    {
+        public static void Main(string[] args)
+        {
+            int[] listPerson = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            Queue<int> list = new Queue<int>(listPerson);
+
+            while (list.Count != 1)
+            {
+                list.Enqueue(list.Dequeue());
+                list.Dequeue();
+            }
+        }
+    }
+}
